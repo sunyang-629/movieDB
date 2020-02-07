5b714d4ac293b13d75fa7a30b83801d9c613a96f
sunyang9876 <sunyang9876@hotmail.com>
2020-02-07 20:24:27 +1000

    movie-database add axios.js

---
+++ src/server/router/movie.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const router = express.Router();
+
+router.get('/api/popular', (req, res) => {
+    
+})
+
+app.listen('3000', () => {
+    console.log('movie app has started');
+})
---
+++ src/server/utils/axios.js
@@ -0,0 +1,11 @@
+const axios = require("axios");
+
+const popularUrl = axios.create({
+  baseURL: "https://api.themoviedb.org/3/discover/movie",
+  params: {
+    api_key: "6ed12e064b90ae1290fa326ce9e790ff",
+    sort_by: "popularity.desc"
+  }
+});
+
+module.exports = popularUrl;
