e16003ad4cee466bb822b69b66866ec47e45d2f8
sunyang9876 <sunyang9876@hotmail.com>
2020-02-08 14:28:29 +1000

    add helmet morgan and airbnb

--- src/server/index.js
+++ src/server/index.js
@@ -1,11 +1,15 @@
-
 const express = require('express');
+const helmet = require('helmet');
+const morgan = require('morgan');
 require('dotenv').config();
+
 const routes = require('./routes');
 
 const PORT = process.env.port || 3000;
 
 const app = express();
+
+app.use(helmet());
 app.use('/api',routes);
 
 app.listen(PORT, () => {
