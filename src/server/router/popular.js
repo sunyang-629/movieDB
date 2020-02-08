e2b5783bdd3b2dc9b4d94cab5afec26adbd8c650
sunyang9876 <sunyang9876@hotmail.com>
2020-02-08 15:43:12 +1000

    movie-database add axios instance

--- src/server/router/popular.js
+++ src/server/router/popular.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const axios = require('axios');
+const axios = require('./../utils/axios');
 
 const api_key = process.env.api_key;
 const router = express.Router();
@@ -7,10 +7,10 @@ const router = express.Router();
 router.get('/', (req, res) => {
     axios({
         method: 'get',
-        url: 'https://api.themoviedb.org/3/discover/movie',
+        url: '/discover/movie',
         params: {
-            api_key: api_key,
-            sort_by: 'popularity.desc'
+            sort_by: 'popularity.desc',
+            api_key: api_key
         }
     })
         .then(response => res.send(response.data.results[0]))
--- src/server/utils/axios.js
+++ src/server/utils/axios.js
@@ -1,11 +1,7 @@
 const axios = require("axios");
 
-const popularUrl = axios.get({
-  baseURL: "https://api.themoviedb.org/3/discover/movie",
-  params: {
-    api_key: "6ed12e064b90ae1290fa326ce9e790ff",
-    sort_by: "popularity.desc"
-  }
+const popularUrl = axios.create({
+  baseURL: "https://api.themoviedb.org/3"
 });
 
 module.exports = popularUrl;
