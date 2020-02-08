5dc2ed404f3b9ad8b4a4edb9c26e577a9ecf2c6e
sunyang9876 <sunyang9876@hotmail.com>
2020-02-08 15:25:15 +1000

    movie-database add notfoundhandler

--- src/server/index.js
+++ src/server/index.js
@@ -4,13 +4,17 @@ const morgan = require('morgan');
 require('dotenv').config();
 
 const routes = require('./routes');
+const notFoundHandler = require('./middleware/notFound')
 
 const PORT = process.env.port || 3000;
 
 const app = express();
 
 app.use(helmet());
-app.use('/api',routes);
+app.use(morgan('dev'));
+
+app.use('/api', routes);
+app.use(notFoundHandler); 
 
 app.listen(PORT, () => {
     console.log(`movie app has started at port ${PORT}`);
