63320b51d70d8d01dce3a13cafd9bc961f20be93
sunyang9876 <sunyang9876@hotmail.com>
2020-02-09 19:06:37 +1000

    movie-database add rating label

--- src/client/components/Card.js
+++ src/client/components/Card.js
@@ -6,6 +6,9 @@ function Card() {
             <a href='#'>
                 <img class="ui medium image" src="https://image.tmdb.org/t/p/w500//9mu13RSsvyDdlyD8TyjuQVrT0gM.jpg" />
             </a>
+            <div className="label">
+                <p>60%</p>
+            </div>
             <h4>name KSDJFKKWJEKJWK JKSDJFLK JKSDJFLKW JKSJDFL</h4>
             <p>time kjsjflajsflkjewljflewfn</p>
         </div>
