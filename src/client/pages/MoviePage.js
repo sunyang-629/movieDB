d0df4696b764df8178736ed833aab0433fa84ebb
sunyang9876 <sunyang9876@hotmail.com>
2020-02-10 13:35:50 +1000

    movie-database add stackable grid for movie page

--- src/client/pages/MoviePage.js
+++ src/client/pages/MoviePage.js
@@ -17,7 +17,12 @@ const MoviePage = props => {
     return (
         <div className="movie">
             <header className="movie__header" style={{ backgroundImage: `url(${state.movie && state.movie.backdrop_path})` }}></header>
-            <div>{state.movie && state.movie.title}</div>
+            <div className="movie__details">
+                <div class="ui stackable two column grid">
+                    <div class="ten wide column">{state.movie && state.movie.title}</div>
+                    <div class="six wide column">{state.movie && state.movie.title}</div>
+                </div>
+            </div>
         </div>
         
         
