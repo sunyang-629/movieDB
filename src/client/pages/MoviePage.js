f2952899098707fe7315afd3d23171b45ebf1d55
sunyang9876 <sunyang9876@hotmail.com>
2020-02-11 16:14:24 +1000

    movie-database add left arrow in page

--- src/client/pages/MoviePage.js
+++ src/client/pages/MoviePage.js
@@ -17,29 +17,33 @@ const MoviePage = props => {
 
     return (
         <div className="movie container">
-         <header className="movie__header" style={{ backgroundImage: `url(${movie.backdrop_path})` }}></header>
-           <div className="movie__details">
-               <div className="container ui centered">
-                   <div className="ui stackable two column grid">
-                       <div className="ten wide column">
-                           <div className="ui grid">
-                               <div className="eight wide column">
-                                   <img className="ui fluid image" src={movie.poster_path} alt="poster" />
-                               </div>
-                               <div className="eight wide column">
-                                   <h1>{movie && movie.title}</h1>
-                                   <p>{movie && movie.release_year} · {movie && movie.vote} User Score</p>
-                                   <p>{movie && movie.runtime_hours}h {movie && movie.runtime_minutes} min</p>
-                               </div>
-                           </div>
-                       </div>
-                       <div className="six wide column">
-                           <h2>Overview</h2>
-                           <p className="movie__details--bolder">{movie && movie.overview}</p>
-                       </div>
-                   </div>
-               </div>
-           </div>
+            <header className="movie__header" style={{ backgroundImage: `url(${movie.backdrop_path})` }}>
+                <div className="back-arrow">
+                    <i class="arrow left icon"></i>
+                </div>
+            </header>
+            <div className="movie__details">
+                <div className="container ui centered">
+                    <div className="ui stackable two column grid">
+                        <div className="ten wide column">
+                            <div className="ui grid">
+                                <div className="eight wide column">
+                                    <img className="ui fluid image" src={movie.poster_path} alt="poster" />
+                                </div>
+                                <div className="eight wide column">
+                                    <h1>{movie && movie.title}</h1>
+                                    <p>{movie && movie.release_year} · {movie && movie.vote} User Score</p>
+                                    <p>{movie && movie.runtime_hours}h {movie && movie.runtime_minutes} min</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div className="six wide column">
+                            <h2>Overview</h2>
+                            <p className="movie__details--bolder">{movie && movie.overview}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
        </div>
     )
 }
