b136228b7c00cfcc20492fb8c3140d96b398c69d
sunyang9876 <sunyang9876@hotmail.com>
2020-02-14 13:02:41 +1000

    movie-database eslint movie.js & popular.js

--- src/server/router/popular.js
+++ src/server/router/popular.js
@@ -4,22 +4,27 @@ const axios = require('./../utils/axios');
 const Popular = require('./../models/Popular');
 const responseFormatter = require('./../utils/responseFormatter');
 
-const api_key = process.env.api_key;
+const { API_KEY: apiKey } = process.env;
 const router = express.Router();
 
 router.get('/', (req, res, next) => {
-    const { page } = req.query;
-    axios({
-        method: 'get',
-        url: '/discover/movie',
-        params: {
-            sort_by: 'popularity.desc',
-            api_key: api_key,
-            page: page,
-        }
-    })
-        .then(response => responseFormatter(res, 200, null, response.data.results.map(movie => new Popular(movie))))
-        .catch(err => next(err));
-})
+  const { page } = req.query;
+  axios({
+    method: 'get',
+    url: '/discover/movie',
+    params: {
+      sort_by: 'popularity.desc',
+      api_key: apiKey,
+      page,
+    },
+  })
+    .then((response) => responseFormatter(
+      res,
+      200,
+      null,
+      response.data.results.map((movie) => new Popular(movie)),
+    ))
+    .catch((err) => next(err));
+});
 
 module.exports = router;
