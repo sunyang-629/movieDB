717998ba0bcadcc02fe1d9f5d3969928d9620656
sunyang9876 <sunyang9876@hotmail.com>
2020-02-14 18:26:28 +1000

    movie-database add constants on server side

--- src/server/models/Movie.js
+++ src/server/models/Movie.js
@@ -1,9 +1,11 @@
+const { POSTER_URL, BACKDROP_URL } = require('../utils/constants');
+
 class Movie {
   constructor(rawData) {
     const { data } = rawData;
     this.title = data.title;
-    this.poster_path = `https://image.tmdb.org/t/p/w500${data.poster_path}`;
-    this.backdrop_path = `https://image.tmdb.org/t/p/w1280${data.backdrop_path}`;
+    this.poster_path = `${POSTER_URL}${data.poster_path}`;
+    this.backdrop_path = `${BACKDROP_URL}${data.backdrop_path}`;
     this.overview = data.overview;
     this.id = data.id;
     this.release_year = data.release_date.slice(0, 4);
---
+++ src/server/utils/constants.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // url
+  POSTER_URL: 'https://image.tmdb.org/t/p/w500',
+  BACKDROP_URL: 'https://image.tmdb.org/t/p/w1280',
+
+};
