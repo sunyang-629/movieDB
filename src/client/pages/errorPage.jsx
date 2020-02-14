ee7e30c836d88e090720b0db29611945e933f63a
sunyang9876 <sunyang9876@hotmail.com>
2020-02-14 15:28:04 +1000

    movie-database complete errorpage

--- src/client/pages/errorPage.jsx
+++ src/client/pages/errorPage.jsx
@@ -6,6 +6,12 @@ const ErrorPage = () => (
     <header className="error__header">
       <Header searchPage={0} />
     </header>
+    <div className="error__body">
+      <div className="container">
+        <h1>Sorry, the page you are looking for does not exist</h1>
+        <h2>Please check the address and try again.</h2>
+      </div>
+    </div>
   </div>
 );
 
