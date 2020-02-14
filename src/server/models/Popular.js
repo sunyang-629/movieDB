43bf120584e5a4177461931737041b7b4f816451
sunyang9876 <sunyang9876@hotmail.com>
2020-02-14 18:28:19 +1000

    movie-database add post_url and backdrop_url in constants

--- src/server/models/Popular.js
+++ src/server/models/Popular.js
@@ -1,3 +1,5 @@
+const { POSTER_URL } = require('../utils/constants');
+
 const calculateMonth = (month) => {
   const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
   return months[parseInt(month, 10) - 1];
@@ -7,7 +9,7 @@ class Popular {
     this.vote = (rawData.vote_average || 0) * 10;
     this.title = rawData.title.toString();
     this.id = (rawData.id || '');
-    this.posterPath = `https://image.tmdb.org/t/p/w500${rawData.poster_path}`;
+    this.posterPath = `${POSTER_URL}${rawData.poster_path}`;
     this.releaseYear = (rawData.release_date || '').slice(0, 4);
     this.releaseMonth = calculateMonth((rawData.release_date || '').slice(5, 7));
   }
