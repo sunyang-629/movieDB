acf7c2196587c988f89e64a8c4a648f15e41ba62
sunyang9876 <sunyang9876@hotmail.com>
2020-02-14 15:45:38 +1000

    movie-database add switch to routes

--- src/client/routes/Routes.jsx
+++ src/client/routes/Routes.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 
 import HomePage from '../pages/HomePage';
 import MoviePage from '../pages/MoviePage';
 import ErrorPage from '../pages/errorPage';
 
 const Routes = () => (
-  <>
+  <Switch>
     <Route exact path="/" component={HomePage} />
     <Route exact path="/movies/:id" component={MoviePage} />
     <Route exact component={ErrorPage} />
-  </>
+  </Switch>
 );
 
 export default Routes;
