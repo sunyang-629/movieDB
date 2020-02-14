7406475c8631eeaac812a3fedb572481bf6b6a26
sunyang9876 <sunyang9876@hotmail.com>
2020-02-14 12:51:44 +1000

    movie-database eslint popular.js

--- src/server/models/Popular.js
+++ src/server/models/Popular.js
@@ -1,18 +1,16 @@
-class Popular{
-    constructor(rawData) {
-        this.vote = (rawData.vote_average || 0) * 10;
-        this.title = rawData.title.toString();
-        this.id = (rawData.id || "");
-        this.posterPath = 'https://image.tmdb.org/t/p/w500' + rawData.poster_path;
-        this.releaseYear = (rawData.release_date || "").slice(0, 4);
-        this.releaseMonth = this.calculateMonth((rawData.release_date || "").slice(5, 7));
-    }
-
-    calculateMonth(month) {
-        const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
-        return months[parseInt(month) - 1]
-    }
+const calculateMonth = (month) => {
+  const months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  return months[parseInt(month, 10) - 1];
+};
+class Popular {
+  constructor(rawData) {
+    this.vote = (rawData.vote_average || 0) * 10;
+    this.title = rawData.title.toString();
+    this.id = (rawData.id || '');
+    this.posterPath = `https://image.tmdb.org/t/p/w500${rawData.poster_path}`;
+    this.releaseYear = (rawData.release_date || '').slice(0, 4);
+    this.releaseMonth = calculateMonth((rawData.release_date || '').slice(5, 7));
+  }
 }
 
 module.exports = Popular;
-
