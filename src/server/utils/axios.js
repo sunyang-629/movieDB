cfa9ce13f0bd5aae01ade09cd7fd795066da1844
sunyang9876 <sunyang9876@hotmail.com>
2020-02-14 13:14:32 +1000

    movie-database eslint axios.js

--- src/server/utils/axios.js
+++ src/server/utils/axios.js
@@ -1,7 +1,7 @@
-const axios = require("axios");
+const axios = require('axios');
 
 const instance = axios.create({
-  baseURL: "https://api.themoviedb.org/3"
+  baseURL: 'https://api.themoviedb.org/3',
 });
 
 module.exports = instance;
