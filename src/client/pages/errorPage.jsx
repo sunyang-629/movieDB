6fa1e611375266d6cec00bbd7c8231b2d4bcd626
sunyang9876 <sunyang9876@hotmail.com>
2020-02-27 21:47:23 +1000

    refactory-components refactory error-page

--- src/client/pages/errorPage.jsx
+++ src/client/pages/errorPage.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
 import Header from '../components/Header';
+import Error from '../components/Error';
 
-const ErrorPage = () => (
-  <div className="error">
-    <Header searchPage={0} />
-    <div className="error__body">
-      <div className="container">
-        <h1>Sorry, the page you are looking for does not exist</h1>
-        <h2>Please check the address and try again.</h2>
-      </div>
+const ErrorPage = () => {
+  return (
+    <div className="error">
+      <Header searchPage={0} />
+      <Error />
     </div>
-  </div>
-);
+  )
+};
 
 export default ErrorPage;
