5ba960e74f6ec981bbb35e30f4fef349e3942170
sunyang9876 <sunyang9876@hotmail.com>
2020-03-02 14:18:58 +1000

    refactory-redux fix searchkey bug after detail page

--- src/client/components/HomePage/Search.jsx
+++ src/client/components/HomePage/Search.jsx
@@ -26,7 +26,7 @@ const Search = (props) => {
   return (
     <div className="ui search popular__header--search centered">
       <div className="ui icon input fluid">
-        <input className="prompt" type="text" placeholder="Search" onChange={(e) => dispatch({type:INPUT_SEARCH_VALUE, value:e.target.value})} />
+        <input className="prompt" value={state.keyword} type="text" placeholder="Search" onChange={(e) => dispatch({type:INPUT_SEARCH_VALUE, value:e.target.value})} />
         <i className="search icon" />
       </div>
       <div className="results" />
