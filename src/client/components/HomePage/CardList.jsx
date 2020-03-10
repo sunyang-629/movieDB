124bb3e8035730e3a55cee60f68c3ba088c4d4e8
sunyang9876 <sunyang9876@hotmail.com>
2020-03-10 16:51:40 +1000

    refactory-eslint fix components/homepage/cardlist

--- src/client/components/HomePage/CardList.jsx
+++ src/client/components/HomePage/CardList.jsx
@@ -1,31 +1,10 @@
-import React, { useReducer, useEffect, useContext } from 'react';
-import PropType from 'prop-types';
+import React, { useContext } from 'react';
 import Card from './Card';
 import HomePageContext from '../../redux/contexts/HomePageContext';
 
-// import {
-//   FETCH_MOVIES,
-//   FETCH_MOVIES_SUCCESS,
-//   FETCH_MOVIES_FAILURE,
-//   FETCH_MORE_MOVIES,
-// } from '../../redux/actions/moviesAction';
-// import { getMovies } from '../../utils/getMovies';
 
 const CardList = () => {
-  const { state, dispatch } = useContext(HomePageContext);
-
-  // useEffect(() => {
-  //   const fetchData = async (state) => {
-  //     dispatch({ type: FETCH_MOVIES });
-  //     try {
-  //       const result = await getMovies(state.page, state.keyword);
-  //       dispatch({ type: (!state.isLoadingMore ? FETCH_MOVIES_SUCCESS : FETCH_MORE_MOVIES), movies: result });
-  //     } catch (error) {
-  //       dispatch({ type: FETCH_MOVIES_FAILURE, error });
-  //     }
-  //   }
-  //   fetchData(state)
-  // }, [state.page,state.keyword]);
+  const { state } = useContext(HomePageContext);
 
   return (
     <div className="container">
@@ -41,16 +20,6 @@ const CardList = () => {
       </div>
     </div>
   );
-};
-CardList.propTypes = {
-  searchState: PropType.bool,
-  popularMovies: {
-    length: PropType.number,
-  },
-};
-CardList.defaultProps = {
-  searchState: false,
-  popularMovies: [],
 };
 
 export default CardList;
