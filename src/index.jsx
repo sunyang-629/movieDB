0136b44410fd90db48c5822a91e0bf28404fdc95
sunyang9876 <sunyang9876@hotmail.com>
2020-03-10 17:40:11 +1000

    refactory-eslint fix index.js

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-import thunkMiddleware from 'redux-thunk';
 import './client/styles/index.scss';
 import App from './App';
 
