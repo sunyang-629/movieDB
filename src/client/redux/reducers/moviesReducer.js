dba8b782cd8863fbca61f328c52a7d3d3ea7ef35
sunyang9876 <sunyang9876@hotmail.com>
2020-03-10 17:39:01 +1000

    refactory-eslint fix redux/reducers/moviesreducer.js

--- src/client/redux/reducers/moviesReducer.js
+++ src/client/redux/reducers/moviesReducer.js
@@ -39,7 +39,7 @@ export const moviesReducer = (state = initialState, action) => {
         error: action.error,
       };
     case FETCH_MORE_MOVIES:
-      state.movies.push.apply(state.movies, action.movies);
+      state.movies.push.apply(...state.movies, action.movies);
       return {
         ...state,
         isLoading: false,
